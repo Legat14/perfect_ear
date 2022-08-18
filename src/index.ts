3b5ae12df51461933ddb845e02d49d6ab35f325f
k0ntinuum <st088265@student.spbu.ru>
2022-08-18 15:30:04 +0300

    fix: address all webpack style loading issues

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-// import './scss/main.scss'; // TODO: Выяснить, почему не работает mini-css-extract plugin
-
-// TODO: Линтер не пропускает импорт без разрешения файла, а webpak не дает вставлять разрешение .ts
-
-import './ts/controllers/sound.ts';
+import './scss/main.scss';
+import './ts/controllers/sound';
 
 console.log('Hello, Perfect ear!');
