51f5b01a0ffcc13acb3dbcc3b80019bc35691e53
Andrei Chaika <redsn@inbox.ru>
2022-08-22 03:26:43 +0300

    fix: attributes in button-builder

--- src/ts/helpers/button-builder.ts
+++ src/ts/helpers/button-builder.ts
@@ -5,17 +5,19 @@ class ButtonBuilder extends NodeBuilder<HTMLButtonElement> {
     parentNode,
     className = '',
     content = '',
+    attributes,
   }: {
     parentNode: HTMLElement | null;
     className?: string;
     content?: string;
+    attributes?: object;
   }) {
     super({
       parentNode,
       tagName: 'button',
       className,
       content,
-      attributes: { type: 'button' },
+      attributes: { ...(attributes || []), type: 'button' },
     });
   }
 }
