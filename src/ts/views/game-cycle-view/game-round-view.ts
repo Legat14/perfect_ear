180b313e1548c96f108e262c04884521d580cbb8
k0ntinuum <st088265@student.spbu.ru>
2022-08-26 19:02:15 +0300

    refactor: refactor game-round-view.ts

--- src/ts/views/game-cycle-view/game-round-view.ts
+++ src/ts/views/game-cycle-view/game-round-view.ts
@@ -5,44 +5,49 @@ import GameRoundEndScreen from './round-end-screen';
 import GameRoundStartScreen from './round-start-screen';
 
 class GameRoundView extends NodeBuilder {
-  startScreen!: GameRoundStartScreen;
+  public startScreen!: GameRoundStartScreen;
 
-  endScreen!: GameRoundEndScreen;
+  public endScreen!: GameRoundEndScreen;
 
-  gameNode!: NodeBuilder<HTMLElement>;
+  public gameNode!: NodeBuilder<HTMLElement>;
 
-  onGameStart!: () => void;
+  public onGameStart!: () => void;
 
-  onGameBack!: () => void;
+  public onGameBack!: () => void;
 
-  constructor(parentNode: HTMLElement) {
+  constructor(parentNode: HTMLElement, terms: IRound['terms']) {
     super({ parentNode });
 
-    this.renderStartScreen();
+    this.renderStartScreen(terms);
   }
 
-  public renderStartScreen() {
-    this.startScreen = new GameRoundStartScreen(this.node);
+  public renderStartScreen(terms: IRound['terms']): void {
+    this.startScreen = new GameRoundStartScreen(this.node, terms);
   }
 
-  public renderGame(quizName: IRound['quizName'], gameName: IQuizGame['gameName']) {
+  public renderGame(quizName: IRound['quizName'], gameName: IQuizGame['gameName']): void {
     this.clear();
 
-    const header = new NodeBuilder({ parentNode: this.node, tagName: 'header' }).node;
-    const backButton = new ButtonBuilder({ parentNode: header, content: '' });
-    const headerGameName = new NodeBuilder({ parentNode: header, content: gameName });
+    const header = new NodeBuilder({
+      parentNode: this.node,
+      tagName: 'header',
+      className: 'quiz-header',
+    }).node;
 
-    this.gameNode = new NodeBuilder({ parentNode: this.node, className: `game ${headerGameName}` });
+    const backButton = new ButtonBuilder({ parentNode: header, className: 'quiz-header__back-btn' }).node;
+    backButton.onclick = () => this.onGameBack();
 
-    backButton.node.onclick = () => this.onGameBack();
+    header.innerHTML += `<div><h1 class="quiz-header__quiz-name">${quizName}</h1><h2 class="quiz-header__game-name">${gameName}</h2></div>`;
+
+    this.gameNode = new NodeBuilder({ parentNode: this.node, className: 'game' });
   }
 
-  public renderQuiz(quiz: HTMLElement) {
+  public renderQuiz(quiz: HTMLElement): void {
     this.gameNode.clear();
     this.gameNode.append(quiz);
   }
 
-  public renderEndScreen(result: IGameResult, nextGameName?: IRound['quizName']) {
+  public renderEndScreen(result: IGameResult, nextGameName?: IRound['quizName']): void {
     this.clear();
     this.endScreen = new GameRoundEndScreen(this.node, result, nextGameName);
   }
