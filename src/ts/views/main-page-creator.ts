9b11dacff2b26b914ba80b4695a765d6ad53602f
Andrei Chaika <redsn@inbox.ru>
2022-08-27 15:45:27 +0300

    fix: delete header in main-page-creatos.ts

--- src/ts/views/main-page-creator.ts
+++ src/ts/views/main-page-creator.ts
@@ -1,18 +1,12 @@
 import ViewsController from '../controllers/viewsController';
 import FooterView from './footer';
 import HeaderView from './header';
-import MainView from './main';
-import WrapperView from './wrapper';
 
 const header = new HeaderView(document.body);
-const main = new MainView(document.body);
-const wrapper = new WrapperView(main.node);
-const viewsController = new ViewsController(wrapper.node);
+const viewsController = new ViewsController(document.body);
 viewsController.init();
 const footer = new FooterView(document.body);
 
 console.log(header);
-console.log(main);
-console.log(wrapper);
 console.log(viewsController);
 console.log(footer);
