b15246b16d4837e9262eb21aeab22b2dc5e32681
Legat <zia-d@mail.ru>
2022-09-02 20:24:19 +0400

    feat: add user statistic handler to refresh counters

---
+++ src/ts/controllers/user-data-handlers/user-statistic-handler.ts
@@ -0,0 +1,68 @@
+import UserProfile from '../../models/user-profile';
+import { IUserStatisticCounters } from '../../types/data-types';
+
+class UserStatisticHandler {
+  private userProfile: UserProfile;
+
+  private dayExercisesCounter: HTMLElement;
+
+  private dayScoreCounter: HTMLElement;
+
+  private dayTimeCounter: HTMLElement;
+  private totalExercisesCounter: HTMLElement;
+  private totalScoreCounter: HTMLElement;
+  private totalTimeCounter: HTMLElement;
+  private totalIntervalGameScoreCount: HTMLElement;
+
+  constructor(userProfile: UserProfile, userDayStatisticCounters: IUserStatisticCounters) {
+    this.userProfile = userProfile;
+    this.dayExercisesCounter = userDayStatisticCounters.dayExercisesCounter;
+    this.dayScoreCounter = userDayStatisticCounters.dayScoreCounter;
+    this.dayTimeCounter = userDayStatisticCounters.dayTimeCounter;
+    this.totalExercisesCounter = userDayStatisticCounters.totalExercisesCounter;
+    this.totalScoreCounter = userDayStatisticCounters.totalScoreCounter;
+    this.totalTimeCounter = userDayStatisticCounters.totalTimeCounter;
+    this.totalIntervalGameScoreCount = userDayStatisticCounters.totalIntervalGameScoreCount;
+    this.refrashCounters();
+  }
+
+  private refreshDayExercisesCounter() {
+    this.dayExercisesCounter.innerHTML = this.userProfile.getDayExercises().toString();
+  }
+
+  private refreshDayScoreCounter() {
+    this.dayScoreCounter.innerHTML = this.userProfile.getDayScore().toString();
+  }
+
+  private refreshDayTimeCounter() {
+    this.dayTimeCounter.innerHTML = this.userProfile.getDayTime().toString();
+  }
+
+  private refreshTotalExercisesCounter() {
+    this.totalExercisesCounter.innerHTML = this.userProfile.getTotalExercises().toString();
+  }
+
+  private refreshtoTalScoreCounter() {
+    this.totalScoreCounter.innerHTML = this.userProfile.getTotalScore().toString();
+  }
+
+  private refreshTotalTimeCounter() {
+    this.totalTimeCounter.innerHTML = this.userProfile.getTotalTime().toString();
+  }
+
+  private refreshTotalIntervalGameScoreCount() {
+    this.totalIntervalGameScoreCount.innerHTML = this.userProfile.getIntervalGameScore().toString();
+  }
+
+  public refrashCounters() {
+    this.refreshDayExercisesCounter();
+    this.refreshDayScoreCounter();
+    this.refreshDayTimeCounter();
+    this.refreshTotalExercisesCounter();
+    this.refreshtoTalScoreCounter();
+    this.refreshTotalTimeCounter();
+    this.refreshTotalIntervalGameScoreCount();
+  }
+}
+
+export default UserStatisticHandler;
